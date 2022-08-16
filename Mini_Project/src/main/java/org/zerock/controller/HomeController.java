57c3f5f6f84ab53f089fb2bf8b610316cfd76a29
wogn9 <dlawogn123@gmail.com>
2022-08-16 15:17:01 +0900

    jh수정

--- Mini_Project/src/main/java/org/zerock/controller/HomeController.java
+++ Mini_Project/src/main/java/org/zerock/controller/HomeController.java
@@ -27,6 +27,7 @@ public class HomeController {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		logger.info("안녕하세요영석입니다");
 		logger.info("안녕하세요영석입니다 STS");
+		logger.info("안녕하세요봉닥입니다");
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
